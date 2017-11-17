21d5823156773c86b27aaa5861f37a7f016a6f89
Aayush <aayushyadav96@gmail.com>
2017-11-17 16:15:52 +0530

    Update text_cnn.py

--- text_cnn.py
+++ text_cnn.py
@@ -1,3 +1,6 @@
+# Currently cross validating and testing various models
+# Variation 1: <2, 1, 1> Conv+Maxpool w/o dropout.
+
 import tensorflow as tf
 import numpy as np
 
@@ -9,13 +12,12 @@ class TextCNN(object):
     """
     def __init__(
       self, sequence_length, num_classes, vocab_size,
-      embedding_size, filter_sizes, num_filters, 
+      embedding_size, filter_sizes, num_filters, learning_rate, 
       l2_reg_lambda=0.0, jac_reg=0.0):
 
-        # Placeholders for input, output and dropout
+        # Placeholders for input and output
         self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
-        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
         # Keeping track of l2 regularization loss (optional)
         l2_loss = tf.constant(0.0)
        
@@ -98,10 +100,6 @@ class TextCNN(object):
         self.h_pool_flat = tf.reshape(self.pooled_3, [-1, num_filters // 2])
         print('Flattened: {}'.format(self.h_pool_flat.get_shape()))
         
-        # Add dropout
-        with tf.name_scope("dropout"):
-            self.h_drop = self.add_dropout(self.h_pool_flat, self.dropout_keep_prob)
-        
         # Final (unnormalized) scores and predictions
         with tf.name_scope("output"):
             with tf.variable_scope("output",  reuse=None):
@@ -112,7 +110,7 @@ class TextCNN(object):
             b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
             l2_loss += tf.nn.l2_loss(W)
             l2_loss += tf.nn.l2_loss(b)
-            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            self.scores = tf.nn.xw_plus_b(self.h_pool_flat, W, b, name="scores")
             
             self.predictions = tf.argmax(self.scores, 1, name="predictions")
         
@@ -141,7 +139,7 @@ class TextCNN(object):
                         reg = tf.matmul(tf.transpose(g_x), g_x)
                                                 
                         # parameter update
-                        W -= 1e-3 * jac_reg * tf.tensordot(reg, W, axes=[[1], [0]])
+                        W -= learning_rate * jac_reg * tf.tensordot(reg, W, axes=[[1], [0]])
 
         # Accuracy
         with tf.name_scope("accuracy"):

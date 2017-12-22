894b78d351ffa255794cbd2d54982e3ddf2de90d
Aayush <aayushyadav96@gmail.com>
2017-12-22 15:59:37 +0530

    Update char_cnn.py

--- char_cnn.py
+++ char_cnn.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 class CharCNN(object):
     """
     A CNN for text classification.
-    based on the Character-level Convolutional Networks for Text Classification paper.
+    based on the Character-level Convolutional Networks for Text Classification.
     """
     def __init__(self, sequence_length, quantization_size, num_classes, filter_sizes, num_filters, 
         learning_rate, l2_reg_lambda=0.0, jac_reg=0.0):
@@ -20,10 +20,12 @@ class CharCNN(object):
         # Keeping track of l2 regularization loss (optional)
         l2_loss = tf.constant(0.0)
 
-        # ================ Layer 1 ================
+        layer_outputs  = []
+        # Layer 1
         with tf.name_scope("conv-maxpool-1"):
             filter_shape = [quantization_size, filter_sizes[0], 1, num_filters]
-            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
+            with tf.variable_scope("conv-maxpool-1",  reuse=None):
+                W = tf.get_variable(shape=filter_shape, initializer=tf.truncated_normal_initializer(stddev=0.05), name="W")
             b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
             conv = tf.nn.conv2d(self.input_x, W, strides=[1, 1, 1, 1], padding="VALID", name="conv1")
             h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
@@ -33,11 +35,13 @@ class CharCNN(object):
                 strides=[1, 1, 3, 1],
                 padding='VALID',
                 name="pool1")
+            layer_outputs.append(pooled)
 
-        # ================ Layer 2 ================
+        # Layer 2
         with tf.name_scope("conv-maxpool-2"):
             filter_shape = [1, filter_sizes[1], num_filters, num_filters]
-            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
+            with tf.variable_scope("conv-maxpool-2",  reuse=None):
+                W = tf.get_variable(shape=filter_shape, initializer=tf.truncated_normal_initializer(stddev=0.05), name="W")
             b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
             conv = tf.nn.conv2d(pooled, W, strides=[1, 1, 1, 1], padding="VALID", name="conv2")
             h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
@@ -47,35 +51,43 @@ class CharCNN(object):
                 strides=[1, 1, 3, 1],
                 padding='VALID',
                 name="pool2")
+            layer_outputs.append(pooled)
 
-        # ================ Layer 3 ================
+        # Layer 3
         with tf.name_scope("conv-3"):
             filter_shape = [1, filter_sizes[2], num_filters, num_filters]
-            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
+            with tf.variable_scope("conv-3",  reuse=None):
+                W = tf.get_variable(shape=filter_shape, initializer=tf.truncated_normal_initializer(stddev=0.05), name="W")
             b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
             conv = tf.nn.conv2d(pooled, W, strides=[1, 1, 1, 1], padding="VALID", name="conv3")
             h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+            layer_outputs.append(h)
 
-        # ================ Layer 4 ================
+        # Layer 4
         with tf.name_scope("conv-4"):
             filter_shape = [1, filter_sizes[3], num_filters, num_filters]
-            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
+            with tf.variable_scope("conv-4",  reuse=None):
+                W = tf.get_variable(shape=filter_shape, initializer=tf.truncated_normal_initializer(stddev=0.05), name="W")
             b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
             conv = tf.nn.conv2d(h, W, strides=[1, 1, 1, 1], padding="VALID", name="conv4")
             h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+            layer_outputs.append(h)
 
-        # ================ Layer 5 ================
+        # Layer 5
         with tf.name_scope("conv-5"):
             filter_shape = [1, filter_sizes[4], num_filters, num_filters]
-            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
+            with tf.variable_scope("conv-5",  reuse=None):
+                W = tf.get_variable(shape=filter_shape, initializer=tf.truncated_normal_initializer(stddev=0.05), name="W")
             b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
             conv = tf.nn.conv2d(h, W, strides=[1, 1, 1, 1], padding="VALID", name="conv5")
             h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+            layer_outputs.append(h)
 
-        # ================ Layer 6 ================
+        # Layer 6
         with tf.name_scope("conv-maxpool-6"):
             filter_shape = [1, filter_sizes[5], num_filters, num_filters]
-            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
+            with tf.variable_scope("conv-maxpool-6",  reuse=None):
+                W = tf.get_variable(shape=filter_shape, initializer=tf.truncated_normal_initializer(stddev=0.05), name="W")
             b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
             conv = tf.nn.conv2d(h, W, strides=[1, 1, 1, 1], padding="VALID", name="conv6")
             h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
@@ -85,8 +97,9 @@ class CharCNN(object):
                 strides=[1, 1, 3, 1],
                 padding='VALID',
                 name="pool6")
+            layer_outputs.append(pooled)
 
-        # ================ Layer 7 ================
+        # Layer 7
         feature_vec_length = 34 * num_filters
         h_pool_flat = tf.reshape(pooled, [-1, feature_vec_length])
 
@@ -94,9 +107,10 @@ class CharCNN(object):
         with tf.name_scope("dropout-1"):
             drop1 = tf.nn.dropout(h_pool_flat, self.dropout_keep_prob)
 
-        # Fully connected layer 1
+        # First fully connected layer
         with tf.name_scope("fc-1"):
-            W = tf.Variable(tf.truncated_normal([feature_vec_length, 1024], stddev=0.05), name="W")
+            with tf.variable_scope("fc-1",  reuse=None):
+                W = tf.get_variable(shape=[feature_vec_length, 1024], initializer=tf.truncated_normal_initializer(stddev=0.05), name="W")
             # W = tf.get_variable("W", shape=[num_features_total, 1024],
             #                     initializer=tf.contrib.layers.xavier_initializer())
             b = tf.Variable(tf.constant(0.1, shape=[1024]), name="b")
@@ -104,15 +118,17 @@ class CharCNN(object):
             # l2_loss += tf.nn.l2_loss(b)
 
             fc_1_output = tf.nn.relu(tf.nn.xw_plus_b(drop1, W, b), name="fc-1-out")
+            layer_outputs.append(fc_1_output)
 
-        # ================ Layer 8 ================
+        # Layer 8
         # Add dropout
         with tf.name_scope("dropout-2"):
             drop2 = tf.nn.dropout(fc_1_output, self.dropout_keep_prob)
 
-        # Fully connected layer 2
+        # Second fully connected layer
         with tf.name_scope("fc-2"):
-            W = tf.Variable(tf.truncated_normal([1024, 1024], stddev=0.05), name="W")
+            with tf.variable_scope("fc-2",  reuse=None):
+                W = tf.get_variable(shape=[1024, 1024], initializer=tf.truncated_normal_initializer(stddev=0.05), name="W")
             # W = tf.get_variable("W", shape=[1024, 1024],
             #                     initializer=tf.contrib.layers.xavier_initializer())
             b = tf.Variable(tf.constant(0.1, shape=[1024]), name="b")
@@ -120,24 +136,55 @@ class CharCNN(object):
             # l2_loss += tf.nn.l2_loss(b)
 
             fc_2_output = tf.nn.relu(tf.nn.xw_plus_b(drop2, W, b), name="fc-2-out")
+            layer_outputs.append(fc_2_output)
 
-        # ================ Layer 9 ================
-        # Fully connected layer 3
-        with tf.name_scope("fc-3"):
-            W = tf.Variable(tf.truncated_normal([1024, num_classes], stddev=0.05), name="W")
+        # Layer 9
+        # Output layer
+        with tf.name_scope("output"):
+            with tf.variable_scope("output",  reuse=None):
+                W = tf.get_variable(shape=[1024, num_classes], initializer=tf.truncated_normal_initializer(stddev=0.05), name="W")
             b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
             # l2_loss += tf.nn.l2_loss(W)
             # l2_loss += tf.nn.l2_loss(b)
 
-            scores = tf.nn.xw_plus_b(fc_2_output, W, b, name="output")
-            predictions = tf.argmax(scores, 1, name="predictions")
-        # ================ Loss and Accuracy ================
-        # CalculateMean cross-entropy loss
+            self.scores = tf.nn.xw_plus_b(fc_2_output, W, b, name="output")
+            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+
+        # Calculate Mean cross-entropy loss
         with tf.name_scope("loss"):
-            losses = tf.nn.softmax_cross_entropy_with_logits(logits=scores, labels=self.input_y)
-            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+            softmax_pred = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            self.loss = tf.reduce_mean(softmax_pred) + l2_reg_lambda * l2_loss
+
+        # Jacobian Regularizer
+        # Currently failing here: slice index 3 of dimension 0 out of bounds ...
+        with tf.name_scope("jacobian-regularizer"):
+            if jac_reg > 0.0:
+                layer_names = ["conv-maxpool-1", "conv-maxpool-2",
+                               "conv-3", "conv-4", "conv-5",
+                               "conv-maxpool-6", "fc-1", "fc-2"]
+                for (idx, scope) in enumerate(layer_names):
+                    with tf.variable_scope(scope, reuse=True):
+                        W = tf.get_variable("W")
+
+                        # jacobian matrix of network output w.r.t. the outputs of layer L
+                        # dimension: (batch_size, width, height, number of filters)
+
+                        # g_x = tf.gradients(tf.reduce_sum(tf.multiply(self.input_y, self.scores)), layer_outputs[idx])
+                        g_x = tf.gradients(tf.multiply(self.input_y,
+                                self.scores), layer_outputs[idx])
+
+                        # reshape (batch_size, height, width, number of filters) to (batch_size * width, number of filters)
+                        g_x = tf.reshape(g_x, shape=[-1,
+                                tf.shape(W)[3]])
+
+                        # covariance matrix of jacobian vectors
+                        reg = tf.matmul(tf.transpose(g_x), g_x)
+
+                        # parameter update
+                        W -= learning_rate * jac_reg \
+                            * tf.tensordot(reg, W, axes=[[1], [0]])
 
         # Accuracy
         with tf.name_scope("accuracy"):
-            correct_predictions = tf.equal(predictions, tf.argmax(self.input_y, 1))
+            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
             self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
